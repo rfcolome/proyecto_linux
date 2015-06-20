48404466f1717609d5168b4d6bf0490f8d82773c
colo <ricardo@generalui.com>
2015-06-20 08:32:05 -0600

    cree un script inicial que no hace nada

---
+++ monitorear_cpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Proyecto de Linux
+# Monitorear el uso de CPU durante un lapso de tiempo determinado
+#
+# Ricardo Francisco Colome Altamirano 00054809
+# Diego Alberto Arana Vaquerano 00017912
+#
+#
+
+exit 0
